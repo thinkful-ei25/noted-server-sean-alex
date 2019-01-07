e612ae319dc97079df9279f89e4dc06ab81c647b
Sean Nealon <signalflowsean@gmail.com>
2019-01-07 11:42:18 -0500

    user schema added

---
+++ models/user.js
@@ -0,0 +1,31 @@
+'use strict'; 
+
+const bcrypt = require('bcrypt')
+const mongoose = require('mongoose'); 
+
+
+const schema = new mongoose.Schema({ 
+  fullname: { type: String, default: '' }, 
+  username: { type: String, required: true, unique: true }, 
+  password: { type: String, required: true}
+}); 
+
+schema.set('toJSON', { 
+  virtual: true, 
+  transform: (doc, result) => { 
+    delete result._id; 
+    delete result.__v; 
+    delete result.password; 
+  }
+});
+
+schema.methods.validatePassword = function (pwd) { 
+  const currentUser = this; 
+  return bcrypt.compare(pwd, currentUser.password); 
+}; 
+
+schema.statics.hashPassword = function (pwd) { 
+  return bcrypt.hash(pwd, 10); 
+}; 
+
+module.exports = mongoose.model('User', schema); 
