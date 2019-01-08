09a285a500dd04f82ca330f686f9df80b2119646
Sean Nealon <signalflowsean@gmail.com>
2019-01-08 18:41:52 -0500

    user can post score

--- routes/score.js
+++ routes/score.js
@@ -3,11 +3,26 @@
 const express = require('express'); 
 const router = express.Router(); 
 
+const User = require('../models/user'); 
+
 router.post('/', (req, res) => { 
-  res.send('HI')
+  const userId = req.user.id; 
+  const { name, definition } = req.body; 
+
+  User.findOne( {userId} )
+    .populate('questions')
+    .then(result => {
+      const answer = result.questions[0]; 
+      const isValid = 
+        (name === answer.name && definition === answer.function) ? 'Correct' : 'Incorrect'; 
+      
+        res.json(isValid); 
+    }); 
 }); 
 
 router.get('/', (req, res) => { 
+  
+  //WILL CHANGE THE ITEM IN THE ARRAY AFTER VALIDATION
   res.send('BYE'); 
 }); 
 
