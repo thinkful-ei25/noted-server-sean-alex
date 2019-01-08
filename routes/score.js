c99e464247b0a633815f5495868500d8693195fe
Sean Nealon <signalflowsean@gmail.com>
2019-01-08 18:43:38 -0500

    merging

--- routes/score.js
+++ routes/score.js
@@ -15,15 +15,8 @@ router.post('/', (req, res) => {
       const answer = result.questions[0]; 
       const isValid = 
         (name === answer.name && definition === answer.function) ? 'Correct' : 'Incorrect'; 
-      
         res.json(isValid); 
     }); 
-}); 
-
-router.get('/', (req, res) => { 
-  
-  //WILL CHANGE THE ITEM IN THE ARRAY AFTER VALIDATION
-  res.send('BYE'); 
 }); 
 
 module.exports = router; 
