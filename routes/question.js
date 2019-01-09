76e45941f72a0a1371ae61d432e023f290fa6997
Sean Nealon <signalflowsean@gmail.com>
2019-01-09 08:26:51 -0500

    the next question goes to the right image

--- routes/question.js
+++ routes/question.js
@@ -6,13 +6,18 @@ const User = require('../models/user');
 
 const router = express.Router(); 
 
+let count = 0; 
+
 router.get('/', (req, res, next) => { 
   const userId = req.user.id;
 
   User.findOne({ userId })
     .populate('questions')
     .then(result => { 
-      const question = result.questions[0]; 
+      count++; 
+      let index = count % 3; 
+      const answer = result.questions[index]; 
+      const question = result.questions[index]; 
       res.json(question); 
     }); 
 }); 
--- routes/score.js
+++ routes/score.js
@@ -5,17 +5,22 @@ const router = express.Router();
 
 const User = require('../models/user'); 
 
+let count = 0; 
 router.post('/', (req, res) => { 
   const userId = req.user.id; 
   const { name, definition } = req.body; 
-
+  
   User.findOne( {userId} )
     .populate('questions')
     .then(result => {
-      const answer = result.questions[0]; 
-      const isValid = 
-        (name === answer.name && definition === answer.function) ? true : false; 
-        res.json(isValid); 
+      count += 1; 
+      let index = count % 3; 
+      const answer = result.questions[index]; 
+      //JUST THE NAME RIGHT NOW
+      // const isValid = 
+      //   (name === answer.name && definition === answer.function) ? true : false; 
+      const isValid = (name === answer.name); 
+      res.json(isValid); 
     }); 
 }); 
 
