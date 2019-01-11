aa3165618321c1ac9720b4161dab1d872add751e
talexcrowell <talexcrowell@gmail.com>
2019-01-11 08:27:03 -0800

    pre-presentation

--- db/data.js
+++ db/data.js
@@ -6,7 +6,7 @@ const questions = [
     img: 'https://classicalmel.files.wordpress.com/2015/10/glissando.jpg',
     name: 'Glissando', 
     description: 'a continuous slide upward or downward between two notes', 
-    memoryStrength: 0,
+    memoryStrength: 1,
     next: 1
   }, 
   { 
@@ -14,7 +14,7 @@ const questions = [
     img: 'https://www.thoughtco.com/thmb/8viKsaXj49Dk5s_fcwBhte3_DRo=/735x0/GL_fermata-music-56a72d325f9b58b7d0e79952.png', 
     name: 'Fermata', 
     description: 'a pause of unspecified length on a note or rest', 
-    memoryStrength: 0,
+    memoryStrength: 1,
     next: 2
   },
   { 
@@ -22,10 +22,75 @@ const questions = [
     img: 'https://d4u3lqifjlxra.cloudfront.net/uploads/example/file/163/breathmark.jpg', 
     name: 'Breath Mark', 
     description: 'take a breath', 
-    memoryStrength: 0,
+    memoryStrength: 1,
+    next: 3
+  }, 
+  { 
+    _id: '111111111111111111111104',
+    img: 'http://www.timusic.net/wp-content/uploads/sforzando.jpg', 
+    name: 'Sforzando', 
+    description: '(especially as a direction) with sudden emphasis', 
+    memoryStrength: 1, 
+    next: 4
+  }, 
+  {
+    _id: '111111111111111111111105', 
+    img: 'https://upload.wikimedia.org/wikipedia/commons/e/e0/Lydian_augmented_scale_on_C.png', 
+    name: 'Lydian', 
+    description: 'the mode represented by the natural diatonic scale F–F (containing an augmented 4th)',
+    memoryStrength: 1, 
+    next: 5
+  }, 
+  { 
+    _id: '111111111111111111111106', 
+    img: 'https://www.uml.edu/Images/notes_tcm18-38391.jpg', 
+    name: 'Shape Notes', 
+    description: 'Shape notes are a variant system of Western musical notation whereby the note heads are printed in distinct shapes to indicate their scale degree and solmization syllable (fa, sol, la, etc.).', 
+    memoryStrength: 1, 
+    next: 6
+  }, 
+  { 
+    _id: '111111111111111111111107', 
+    img: 'https://www.thoughtco.com/thmb/V01b-ZIUXfPPjU7hLOde1k8rmRk=/640x528/filters:no_upscale()/Sustain-PIANO_MUSIC-56a72ea53df78cf7729318b2.png', 
+    name: 'Sustain', 
+    description: 'A sustain pedal or sustaining pedal is the most commonly used pedal in a modern piano. It is typically the rightmost of two or three pedals. When pressed, the sustain pedal "sustains" all the damped strings on the piano by moving all the dampers away from the strings and allowing them to vibrate freely.', 
+    memoryStrength: 1, 
+    next: 7
+  }, 
+  { 
+    _id: '111111111111111111111107', 
+    img: 'http://www.bassflute.co.uk/05-percussive-effects-and-articulation/images/flutter-tonguing.png', 
+    name: 'Flutter Tongue', 
+    description: 'Flutter-tonguing is a wind instrument tonguing technique in which performers flutter their tongue to make a characteristic "FrrrrFrrrrr" sound.', 
+    memoryStrength: 1, 
+    next: 8
+  }, 
+  { 
+    _id: '111111111111111111111108', 
+    img: 'https://cdn3.iconfinder.com/data/icons/music-notes-symbols/512/Icon_54-512.png', 
+    name : 'Marcato', 
+    description: '(especially as a direction) with emphasis.', 
+    memoryStrength: 1, 
+    next: 9
+  }, 
+  { 
+    _id: '111111111111111111111109', 
+    img: 'https://i.pinimg.com/736x/4f/4b/90/4f4b90d225924ed17d117dd1655c181d--music-symbols.jpg', 
+    name: 'Tenuto', 
+    description: '(especially as a direction) with a note or chord held for its full time value or slightly more.', 
+    memoryStrength: 1, 
+    next: 10
+  }, 
+  { 
+    _id: '111111111111111111111110', 
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Middle_C_pizz.png/220px-Middle_C_pizz.png', 
+    name: 'Pizzicato', 
+    description: 'used as a direction in music to indicate that notes should be played by plucking the strings of a violin, viola, cello, etc., with the fingers instead of by using a bow', 
+    memoryStrength: 1, 
     next: null
   }
-]; 
+
+];  
 
 module.exports = questions; 
 
--- routes/metric.js
+++ routes/metric.js
@@ -4,12 +4,16 @@ const express = require('express');
 const router = express.Router(); 
 const User = require('../models/user'); 
 
-router.get('/', (req, res) => { 
+router.get('/', (req, res, next) => { 
   const username = req.user.username;
 
   User.findOne({ username })
     .then(result => {
       const userSessions = result.sessions;
+      
+      if (userSessions.length ===0){
+        return res.json({lastSessionAvg: 0});
+      }
 
       let sumSessions =0;
       let sumOfQuestions =0;
@@ -28,11 +32,13 @@ router.get('/', (req, res) => {
       let recentAvg = result.sessions[result.sessions.length-1].sessionAvg;
       console.log({lastSessionScore: recent, lastSessionAvg: recentAvg, allSessionsAvg: allSessionsAvg});
       res.json({lastSessionScore: recent, lastSessionAvg: recentAvg, allSessionsAvg: allSessionsAvg});
-    });
+    })
+    .catch(err => next(err));
 
 }); 
 
 router.post('/startSession', (req, res) => { 
+  
   const username = req.user.username; 
 
   User.findOne({ username })
@@ -41,9 +47,12 @@ router.post('/startSession', (req, res) => {
       const sessions = result.sessions; 
       const sessionsCopy = [...sessions]; 
       sessionsCopy.push([]); 
-      result.sessions = sessionsCopy; 
-      result.save(); 
+      result.sessions = sessionsCopy;
+      result.save();
+      console.log('End');
+      return res.json({message: 'DUDE'});
     })
+    .then(result => res.status(200))
     .catch(err => { 
       console.error('err', err); 
     }); 
@@ -75,6 +84,7 @@ router.post('/endSession', (req, res) => {
       userSessions[userSessions.length- 1].sessionAvg = parseFloat(sumAnswers / userSessions[userSessions.length- 1].answers.length);
       userSessions[userSessions.length- 1].sumScore = sumAnswers;     
       result.save();
+      return res.json({message: 'DUDEZOR'});
     })
     .catch(err => { 
       console.error('err', err); 
